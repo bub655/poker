b5ee6a5604ad70b46752cdfe802632e12ba3c129
bub655 <52846238+bub655@users.noreply.github.com>
2024-05-30 16:20:15 -0700

    Delete files and convert to typescript
    Friend wanted to change language from python to typescript.

--- card.py
+++
@@ -1,7 +0,0 @@
-class Card:
-    def __init__(self, rank, suit):
-        self.suit = suit
-        self.rank = rank
-
-    def toString(self):
-        return "" + self.rank + " " + self.suit
--- poker.py
+++
@@ -1,8 +0,0 @@
-class playPoker:
-    deck = []
-    suits = ["D", "C", "H", "S"]
-    tokens = 1000
-    def generateDeck():
-        for i in range(13) :
-            for j in suits:
-                deck.add(new Card(i, suits[j]))
